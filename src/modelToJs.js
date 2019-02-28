0462f31ccdb17cc79e5364f4daae77b62fde1b0b
Sven Helmberger <sven.helmberger@quinscape.de>
2019-02-28 16:48:17 +0100

    Early sketch for modelToJs
    We add a minimal sketch for how to iterate over all JSON files in the
    automaton model directory and how to categorize them with the newly
    refactored matchPath.js module.
    
    WIP

--- src/modelToJs.js
+++ src/modelToJs.js
@@ -1,2 +1,99 @@
 #!/usr/bin/env node
-//console.log("modelToJs not implemented yet");
+import path from "path"
+import recursiveReadDir from "recursive-readdir"
+import matchPath from "./matchPath";
+
+const MODEL_PATH = "./src/main/webapp/WEB-INF/automaton/apps";
+const APPS_INFIX = "/apps/";
+
+function toInternalPath(fileName)
+{
+    const pos = fileName.indexOf(APPS_INFIX);
+    if (pos < 0)
+    {
+        console.log("Ignore ", fileName, " for now");
+        return null;
+    }
+
+    return fileName.substr(pos + APPS_INFIX.length);
+}
+
+function getFirstSegment(path)
+{
+    const pos = path.indexOf("/");
+    if (pos >= 0)
+    {
+        return path.substr(0, pos);
+    }
+
+    throw new Error("Path has no slash");
+}
+
+
+recursiveReadDir(MODEL_PATH, [], function (err, fileNames) {
+
+    if (err)
+    {
+        console.error(err);
+        process.exit(1);
+    }
+
+    for (let i = 0; i < fileNames.length; i++)
+    {
+        const internalPath = toInternalPath(fileNames[i])
+        if (internalPath === null)
+        {
+            continue;
+        }
+
+        const appName = getFirstSegment(internalPath);
+
+        const inAppPath = "." + internalPath.substr(appName.length);
+        const { processName, shortName, isDomain, isComposite } = matchPath(inAppPath);
+
+        if (isDomain)
+        {
+            convertDomainModel(internalPath);
+        }
+        else if (processName != null)
+        {
+            if (isComposite)
+            {
+                convertComposite(internalPath);
+            }
+            else
+            {
+                convertProcessExport(internalPath);
+            }
+        }
+        else
+        {
+            convertMisc(internalPath);
+        }
+
+
+        //console.log({appName, domainName, processName, shortName, isComposite});
+
+    }
+
+});
+
+function convertDomainModel(internalPath)
+{
+    console.log("convertDomainModel", internalPath)
+}
+
+function convertComposite(internalPath)
+{
+    console.log("convertComposite", internalPath)
+}
+
+function convertProcessExport(internalPath)
+{
+    console.log("convertProcessExport", internalPath)
+}
+
+function convertMisc(internalPath)
+{
+    console.log("convertMisc", internalPath)
+}
