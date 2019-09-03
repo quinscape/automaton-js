62cbf2496fd770d1f86c39566a70266e67357f84
Sven Helmberger <sven.helmberger@quinscape.de>
2019-09-03 12:10:26 +0200

    Implement <AssociationSelector/>
    Implements the new <AssociationSelector/> selector component that
    renders a bootstrap 4 list group for every entity the current entity is
    connected to via the associative entity.
    
    Here we see an example of an associative entity "BazLink" as
    many-to-many connection between the associated domain types "Baz" and
    "BazValue"
    
        "Associated Entity A (see BazLink)"
        type Baz {
            "Many-to-many objects from 'baz_link.baz_id'"
            bazLinks: [BazLink]!
            "DB column 'id'"
            id: String!
            "DB column 'name'"
            name: String!
        }
    
        "Associative entity m-to-n connecting Baz and BazValue"
        type BazLink {
            "Target of 'baz_id'"
            baz: Baz!
            "DB foreign key column 'baz_id'"
            bazId: String!
            "DB column 'id'"
            id: String!
            "Target of 'value_id'"
            value: BazValue!
            "DB foreign key column 'value_id'"
            valueId: String!
        }
    
        "Associated Entity B (see BazLink)"
        type BazValue {
            "Many-to-many objects from 'baz_link.value_id'"
            bazLinks: [BazLink]!
            "DB column 'id'"
            id: String!
            "DB column 'name'"
            name: String!
        }
    
    From the "Baz" size, the <AssociationSelector/> is used like this
    
        <AssociationSelector
            name="bazLinks"
            label="Associated Values"
            display="value.name"
            value="value.id"
            helpText="Select associated baz values"
            query={
                Q_BazValueList
            }
    
        />
    
    The "Baz" object the form is based on must select all its bazLinks
    objects including the embedded object of the other side ("value" from
    the "Baz" side)
    
    The Q_BazValueList query queries "BazValue" objects without the embedded
    bazLinks.
    
    As it is now, the InteractiveQuery mechanism is not able to follow the
    many-to-many connections (we might add that later)
    
    At this point, you need a decidated non-iQuery end-point to fetch "Baz"
    object including the bazLinks.value sub-objects.

---
+++ src/ui/AssociationSelectorModal.js
@@ -0,0 +1,89 @@
+import React from "react"
+import i18n from "../i18n";
+
+import { ButtonToolbar, Container, Modal, ModalBody, ModalHeader } from "reactstrap"
+import DataGrid from "./datagrid/IQueryGrid";
+import get from "lodash.get"
+import { observer as fnObserver, useObserver } from "mobx-react-lite";
+
+
+/**
+ * Selector popup for the FK selector.
+ */
+const AssociationSelectorModal = fnObserver(props => {
+
+    const { isOpen, iQuery, columns, title, toggle, fade, selected, idPath } = props;
+
+    return(
+        <Modal isOpen={ isOpen } toggle={ toggle } size="lg" fade={ fade }>
+            <ModalHeader
+                toggle={ toggle }
+            >
+                {
+                    title
+                }
+            </ModalHeader>
+            <ModalBody>
+                <Container fluid={ true }>
+                    {
+                        isOpen && (
+                            <DataGrid
+                                id="fk-selector-grid"
+                                tableClassName="table-hover table-striped table-bordered table-sm table-responsive"
+                                value={ iQuery }
+                            >
+                                <DataGrid.Column
+                                    heading={ "Selection" }
+                                >
+                                    {
+                                        entity => (
+                                            <DataGrid.RowSelector
+                                                id={ get(entity, idPath) }
+                                                selectedValues={ selected }
+                                            />
+                                        )
+                                    }
+                                </DataGrid.Column>
+                                {
+                                    columns.map(
+                                        name => (
+                                            <DataGrid.Column
+                                                key={ name }
+                                                name={ name }
+                                                filter="containsIgnoreCase"
+                                            />
+                                        )
+                                    )
+                                }
+                            </DataGrid>
+                        )
+                    }
+                    <ButtonToolbar>
+                        <button
+                            type="button"
+                            className="btn btn-secondary mr-1"
+                            onClick={ ev => selected.clear() }
+                        >
+                            {
+                                i18n("Unselect All")
+                            }
+                        </button>
+                        <button
+                            type="button"
+                            className="btn btn-secondary mr-1"
+                            onClick={ toggle }
+                        >
+                            {
+                                i18n("Close")
+                            }
+                        </button>
+                    </ButtonToolbar>
+                </Container>
+            </ModalBody>
+        </Modal>
+    )
+});
+
+AssociationSelectorModal.displayName = "AssociationSelectorModal";
+
+export default AssociationSelectorModal
