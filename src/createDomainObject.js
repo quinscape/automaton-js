b6aa329dab0c8856664591f1989d8e7533e7acc2
Sven Helmberger <sven.helmberger@quinscape.de>
2019-10-25 13:39:55 +0200

    createDomainObject: Add optional id
    Adds id as optional second parameter with the default of using
    uuid.v4() like before.

--- src/createDomainObject.js
+++ src/createDomainObject.js
@@ -6,12 +6,11 @@ import uuid from "uuid";
  * Creates a new domain object with a new UUID.
  *
  * @param {String} type      domain type name
+ * @param {*} [id]           id for the new object (default creates a new UUID)
  * @return {object}  domain object
  */
-export default function createDomainObject(type)
+export default function createDomainObject(type, id = uuid.v4())
 {
-    const id = uuid.v4();
-
     const newObj = getWireFormat().classes[type];
 
     if (!newObj)
