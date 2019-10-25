ab0c5244a771a47c3913574462a9a877be2dedaf
Sven Helmberger <sven.helmberger@quinscape.de>
2019-10-25 13:42:13 +0200

    extractTypeData: check type
    Throw a meaningful error when the user feeds in a non-existing type.

--- src/extractTypeData.js
+++ src/extractTypeData.js
@@ -4,6 +4,11 @@ export default function extractTypeData(typeName, obj)
 {
     const typeDef = config.inputSchema.getType(typeName);
 
+    if (!typeDef)
+    {
+        throw new Error("Cannot extract data of type '" + typeName + "': Type not found in schema.")
+    }
+    
     const isInputType = !!typeDef.inputFields;
 
     const fields = isInputType ? typeDef.inputFields : typeDef.fields;
