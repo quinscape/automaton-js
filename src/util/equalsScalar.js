e5d44a4040eba396312eba2d56892391a2e8c532
Sven Helmberger <sven.helmberger@quinscape.de>
2020-05-07 15:31:07 +0200

    Add equalsScalar
    Adds a new method for the equality comparison of scalar values, which we
    have been missing so far.
    
    For most scalar types this is a simple === comparison, but the Date
    based scalar types already need to compared the respective time values
    via .getTime().
    
    Other types might have other specific equality rules.
    
    So we add an util model util/equalsScalar.js which has the equality
    method as main method and also offers a named export
    
    registerScalarEquals(scalarType, fn)
    
    with which applications can define custom equality functions for their
    custom scalar types.
    
    This module fixes a reference error in the previous release

---
+++ src/util/equalsScalar.js
@@ -0,0 +1,78 @@
+/**
+ * Compares for instance equality
+ * @param {*} a      date a
+ * @param {*} b      date b
+ *
+ * @returns {boolean}   true if a equals b
+ */
+function identityEquals(a, b)
+{
+    return a === b;
+}
+
+
+/**
+ * Compares dates for equality
+ * @param {Date} a      date a
+ * @param {Date} b      date b
+ *
+ * @returns {boolean}   true if a equals b
+ */
+function dateEquals(a, b)
+{
+    const timeA = a ? a.getTime() : -1;
+    const timeB = b ? b.getTime() : -1;
+    return timeA === timeB;
+}
+
+
+/**
+ * Default scalar equality configuration
+ */
+const equalsImpls = {
+    "Int": identityEquals,
+    "Float": identityEquals,
+    "String": identityEquals,
+    "Boolean": identityEquals,
+    "ID": identityEquals,
+    "Long": identityEquals,
+    "Short": identityEquals,
+    "Byte": identityEquals,
+    "BigInteger": identityEquals,
+    "BigDecimal": identityEquals,
+    "Char": identityEquals,
+    "Timestamp": dateEquals,
+    "Date": dateEquals
+};
+
+
+/**
+ * Registers a new equality function for the given scalar type.
+ *
+ * @param {String} scalarType   scalar type
+ * @param {Function} fn         equality functions
+ */
+export function registerScalarEquals(scalarType, fn)
+{
+    equalsImpls[scalarType] = fn;
+}
+
+
+/**
+ * Returns true when the given scalar values are equal. The equality rules are scalar type dependent.
+ * Many scalar types use instance equality ( === ), while e.g. the Date based types compare time stamps.
+ *
+ * @param {String} scalarType   scalar type name
+ * @param {*} a                 scalar value a
+ * @param {*} b                 scalar value b
+ *
+ * @returns {boolean}   true if scalar values are equal
+ */
+export default function equalsScalar(scalarType, a, b) {
+    const eq = equalsImpls[scalarType];
+    if (!eq)
+    {
+        throw new Error(`No equals registered for '${scalarType}'`);
+    }
+    return eq(a, b);
+}
