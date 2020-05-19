39a6a0f24137b5d185f61933d470280a05f9b4b7
Mahmoud Hegazy <Mahmoud.Hegazy@quinscape.de>
2020-05-19 14:07:45 +0200

    Created validation schema for Json files
    -created schema to valid the json file before start
    model-to-js process using Ajv package.
    -using json-source-map to detect the line where the
    error is.

---
+++ src/model2js/schemaDeclaration.js
@@ -0,0 +1,343 @@
+export const modelToJsSchema = {
+    "properties":{
+        "importDeclarations":{
+            "type":["array","null"],
+            "items":{
+                "type": ["object","null"],
+                "properties":{
+                    "type": {
+                        "type": "string"
+                    },
+                    "source":{
+                        "type" : "string"
+                    },
+                    "specifiers":{
+                        "type": ["array","null"],
+                        "minItems": 1,
+                        "items":{
+                            "type": ["object","null"],
+                            "properties":{
+                                "type": {
+                                    "type": "string"
+                                },
+                                "name":{
+                                    "type":"string"
+                                },
+                                "aliasOf":{
+                                    "type":"string"
+                                }
+                            },
+                            "required": ["name"]
+                        }
+                    }
+                }
+            }
+        },
+        "domain":{
+            "type":["object","null"],
+            "properties":{
+                "name":{
+                    "type": "string"
+                },
+                "observables":{
+                    "type": ["array","null"],
+                    "minItems": 1,
+                    "items":{
+                        "type": ["object","null"],
+                        "properties":{
+                            "name": {
+                                "type": "string"
+                            },
+                            "defaultValue":{
+                                "type":["string","null"]
+                            },
+                            "description":{
+                                "type":["string","null"]
+                            }
+                        }
+                    }
+                },
+                "actions":{
+                    "type": ["array","null"]
+                },
+                "computeds":{
+                    "type": ["array","null"],
+                    "minItems": 1,
+                    "items":{
+                        "type": ["object","null"],
+                        "properties":{
+                            "name": {
+                                "type": "string"
+                            },
+                            "code":{
+                                "type":"string"
+                            }
+                        }
+                    }
+                },
+                "helpers":{
+                    "type": "array"
+                },
+            }
+        },
+        "query":{
+            "type":["object","null"],
+            "properties":{
+                "query":{
+                    "type": "string"
+                },
+                "variables":{
+                    "type": ["object","null"],
+                    "properties":{
+                        "config":{
+                            "type": ["object","null"],
+                            "properties":{
+                                "pageSize":{
+                                    "type": ["number","null"]
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        "composite":{
+            "type":["object","null"],
+            "properties":{
+                "type":{
+                    "type":["string","null"]
+                },
+                "constants":{
+                    "type":["array","null"],
+                    "items":{
+                        "type": ["object","null"],
+                        "properties":{
+                            "type": {
+                                "type": "string"
+                            },
+                            "kind": {
+                                "type": "string"
+                            },
+                            "declarations":{
+                                "type": ["array","null"],
+                                "items":{
+                                    "type": ["object","null"],
+                                    "properties":{
+                                        "type":{
+                                            "type":["string","null"]
+                                        },
+                                        "id": {
+                                            "type":["object","null"],
+                                            "properties":{
+                                                "type": {
+                                                    "type": "string"
+                                                },
+                                                "name": {
+                                                    "type": "string"
+                                                },
+                                                "properties": {
+                                                    "type": ["array","null"],
+                                                    "items":{
+                                                        "type":["object","null"],
+                                                        "properties":{
+                                                            "type":{
+                                                                "type":"string"
+                                                            },
+                                                            "key":{
+                                                                "type":"string"
+                                                            },
+                                                            "value":{
+                                                                "type":["object","null"],
+                                                                "properties":{
+                                                                    "type": {
+                                                                        "type": "string"
+                                                                    },
+                                                                    "name": {
+                                                                        "type": "string"
+                                                                    }
+                                                                }
+                                                            },
+                                                        }
+                                                    }
+                                                },
+                                            }
+                                        },
+                                        "init":{
+                                            "type":"string"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                "root":{
+                    "type":["object","null"],
+                    "properties":{
+                        "type": {
+                            "type": "string"
+                        },
+                        "name":{
+                            "type":"string"
+                        },
+                        "attrs":{
+                            "type":["array","null"]
+                        },
+                        "kids":{
+                            "type":["array","null"],
+                            "items":{
+                                "type":["object","null"],
+                                "properties":{
+                                    "name":{
+                                        "type":"string"
+                                    },
+                                    "attrs":{
+                                        "type":["array","null"],
+                                        "items":{
+                                            "type":["object","null"],
+                                            "properties":{
+                                                "type":{
+                                                    "type":"string"
+                                                },
+                                                "name":{
+                                                    "type":"string"
+                                                },
+                                                "value":{
+                                                    "type":["object","null"],
+                                                    "properties":{
+                                                        "type":{
+                                                            "type":["string","null"]
+                                                        },
+                                                        "code":{
+                                                            "type":["string","null"]
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    },
+                                    "value":{
+                                        "type":["string","null"]
+                                    },
+                                    "code":{
+                                        "type":["string","null"]
+                                    },
+                                    "type":{
+                                        "type":["string","null"]
+                                    },
+                                    "params":{
+                                        "type":["array","null"],
+                                        "items":{
+                                            "type":["object","null"],
+                                            "properties":{
+                                                "type":{
+                                                    "type":"string"
+                                                },
+                                                "name":{
+                                                    "type":"string"
+                                                },
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        "export":{
+            "type":["string","null"]
+        },
+        "processExports":{
+            "type":["object","null"],
+            "properties":{
+                "type":{
+                    "type":"string"
+                },
+                "configuration":{
+                    "type":["array","null"]
+                },
+                "process":{
+                    "type":["object","null"],
+                    "properties":{
+                        "startState":{
+                            "type":"string"
+                        },
+                        "states":{
+                            "type":["object","null"]
+                        }
+                    }
+                },
+                "scope":{
+                    "type":["object","null"],
+                    "properties":{
+                        "name":{
+                            "type":"string"
+                        },
+                        "observables":{
+                            "type":["array","null"],
+                            "items":{
+                                "type":["object","null"],
+                                "properties":{
+                                    "name":{
+                                        "type":"string"
+                                    },
+                                    "defaultValue":{
+                                        "type":["string","null"]
+                                    }
+                                }
+                            }
+                        },
+                        "actions":{
+                            "type":["array","null"],
+                            "items":{
+                                "type":["object","null"],
+                                "properties":{
+                                    "name":{
+                                        "type":"string"
+                                    },
+                                    "params":{
+                                        "type":["array","null"],
+                                        "items":{
+                                            "type":["string","null"]
+                                        }
+                                    },
+                                    "code":{
+                                        "type":["string","null"]
+                                    },
+                                    "bound":{
+                                        "type":["boolean","null"]
+                                    }
+                                }
+                            }
+                        },
+                        "computeds":{
+                            "type":["array","null"]
+                        },
+                        "helpers":{
+                            "type":["array","null"],
+                            "items":{
+                                "type":["object","null"],
+                                "properties":{
+                                    "name":{
+                                        "type":"string"
+                                    },
+                                    "defaultValue":{
+                                        "type":["string","null"]
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                "extraConstants":{
+                    "type":["array","null"],
+                    "items":{
+                        "type":["string","null"]
+                    }
+                }
+            }
+        }
+    }
+};
