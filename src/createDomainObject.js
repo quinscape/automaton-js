701eaff184fb495d3bd514ce1861f68c1b73bc4b
Sven Helmberger <sven.helmberger@quinscape.de>
2020-09-22 17:06:06 +0200

    Extend createDomainObject()
    As a convenience function to not have to define defaults for all
    properties that can't be `undefined`, we go and set all non-id fields of
    a newly created domain object to `null` or to an empty array if it's a
    list.
    
    These act as convenient default values for form elements that should
    interpret null to be an empty field. The new revalidate feature
    makes sure the user actually enters meaningful values and fulfills all
    not null contracts in the end.

--- src/createDomainObject.js
+++ src/createDomainObject.js
@@ -1,6 +1,9 @@
+import config from "./config";
 import { getWireFormat } from "./domain";
 import uuid from "uuid";
 import { observable } from "mobx";
+import { getFields, unwrapNonNull } from "./util/type-utils";
+import { LIST } from "domainql-form/lib/kind";
 
 
 /**
@@ -12,20 +15,49 @@ import { observable } from "mobx";
  */
 export default function createDomainObject(type, id = uuid.v4())
 {
+    const typeRef = config.inputSchema.getType(type);
+
+    if (typeRef === null)
+    {
+        throw new Error("Unknown type: " + type);
+    }
+
     const DomainClass = getWireFormat().classes[type];
 
+
+    let instance;
     if (!DomainClass)
     {
-        return observable({
+        instance = observable({
             _type: type,
             id
         });
     }
     else
     {
-        const newObj = new DomainClass();
-        newObj._type = type;
-        newObj.id = id;
-        return newObj;
+        instance = new DomainClass();
+        instance._type = type;
+        instance.id = id;
     }
+
+    const fields = getFields(typeRef);
+
+    for (let i = 0; i < fields.length; i++)
+    {
+        const { name, type } = fields[i];
+
+        if (name !== "id")
+        {
+            if (unwrapNonNull(type).kind === LIST)
+            {
+                instance[name] = [];
+            }
+            else
+            {
+                instance[name] = null;
+            }
+        }
+    }
+
+    return instance;
 }
