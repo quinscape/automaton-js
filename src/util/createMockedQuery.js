f17464fbe11375ae0ae0304c4adc3147cd61aae9
Sven Helmberger <sven.helmberger@quinscape.de>
2021-03-18 18:31:01 +0100

    Refactor out in-memory query
    We already had an implementation of the interactive query mechanism
    based off a source iquery document containing all data.
    
    We refactor this method out to make it accessible for other purposes,
    and fix a few edge cases while we're at it.
    
    The new method is
    
        import { evaluateMemoryQuery } from "@quinscape/automaton-js"
    
        // ...
    
        const doc = evaluateMemoryQuery(
            wireFormat,
            sourceIQueryDoc,
            queryConfig
        )
    
    By default, the method returns a new InteractiveQuery instance, but you
    can also pass one in as fourth argument to be updated instead.

--- src/util/createMockedQuery.js
+++ src/util/createMockedQuery.js
@@ -1,10 +1,8 @@
 // creates a GraphQLQuery instance with mocked .execute method that returns a fixed result.
-import config from "../config";
 import GraphQLQuery from "../GraphQLQuery";
-import { toJS } from "mobx";
 import { FieldResolver, filterTransformer } from "../index";
 import { field, Type } from "../FilterDSL";
-import { lookupType } from "./type-utils";
+import { evaluateMemoryQuery } from "./evaluateMemoryQuery";
 
 
 /**
@@ -12,9 +10,9 @@ import { lookupType } from "./type-utils";
  *
  * If you need filtering or pagination on your mock, use createFilteredMockQuery
  *
- * @param {WireFormat} format   wire format
+ * @param format   wire format
  * @param {String} type         iQuery container type
- * @param {Object} payload      constant iQuery result to return from the mocked query
+ * @param {Object|function} payload      constant iQuery result to return from the mocked query or function producing a result from given variables
  *
  * @return {GraphQLQuery} mocked query object.
  */
@@ -68,108 +66,6 @@ export function createMockedQuery(format, type, payload)
     return instance;
 }
 
-function transformSortFields(resolver, sortFields)
-{
-    const isDescending = new Array(sortFields.length);
-    const filters = sortFields.map((condition, idx) => {
-
-        if (!condition)
-        {
-            throw new Error("Invalid sort field: " + condition);
-        }
-
-        if (typeof condition === "string")
-        {
-            const desc = condition[0] === "!";
-            isDescending[idx] = desc;
-            const ref = field(desc ? condition.substr(1) : condition);
-
-            return filterTransformer(
-                ref,
-                resolver.resolve
-            );
-        }
-        else
-        {
-            const {type, name} = condition;
-
-            if (type === Type.OPERATION)
-            {
-                const operand = condition.operands[0];
-
-
-                if (name === "asc")
-                {
-                    isDescending[idx] = false;
-                    return filterTransformer(
-                        operand,
-                        resolver.resolve
-                    );
-                }
-                else if (name === "desc")
-                {
-                    isDescending[idx] = true;
-                    return filterTransformer(
-                        operand,
-                        resolver.resolve
-                    );
-                }
-                isDescending[idx] = false;
-            }
-
-            return filterTransformer(
-                condition,
-                resolver.resolve
-            );
-        }
-    })
-
-    return { filters, isDescending }
-}
-
-
-export function sort(rows, sortFields)
-{
-    const resolver = new FieldResolver();
-    const { filters, isDescending } = transformSortFields(resolver, sortFields);
-
-    const numFilters = filters.length;
-    const sortedRows = rows.slice();
-    sortedRows.sort((a, b) => {
-
-        let offset = 0;
-        do
-        {
-            const mul = isDescending[offset] ? -1 : 1;
-
-            const filter = filters[offset];
-
-
-            resolver.current = a;
-            const valueA = filter();
-
-            resolver.current = b;
-            const valueB = filter();
-
-            if (valueA < valueB)
-            {
-                return -1 * mul;
-            }
-            else if (valueA > valueB)
-            {
-                return mul;
-            }
-            else
-            {
-                offset++;
-            }
-
-        } while (offset < numFilters)
-        return 0;
-    })
-
-    return sortedRows;
-}
 
 
 /**
@@ -184,44 +80,10 @@ export function createFilteredMockQuery(format, type, payload)
 
         const config = vars.config || payload.queryConfig;
 
-        const { offset, pageSize } = config;
-
-        const Impl = format.classes[type];
-
-        const newPayload = Impl ? new Impl() : {};
-
-        newPayload.type = payload.type;
-        newPayload.queryConfig = config;
-        newPayload.columnStates = payload.columnStates;
-        newPayload.rowCount = payload.rowCount;
-
-        const resolver = new FieldResolver();
-        const filter = filterTransformer(config.condition, resolver.resolve )
-
-        let filteredRows = payload.rows.filter( r => {
-            resolver.current = r;
-            return filter();
-        })
-
-        if (Array.isArray(config.sortFields) && config.sortFields.length)
-        {
-            filteredRows = sort(filteredRows, config.sortFields);
-        }
-
-        if (pageSize > 0)
-        {
-            newPayload.rows = filteredRows.slice(offset, offset + pageSize)
-        }
-        else
-        {
-            newPayload.rows = filteredRows
-        }
-
-        return newPayload;
+        const doc = evaluateMemoryQuery(format, payload, config);
+        // normally this is done by preprocessor
+        doc._query = query;
+        return doc;
     });
-
-    // normally this is done by preprocessor
-    query._query = query;
-
     return query
 }
