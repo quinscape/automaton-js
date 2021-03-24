a124d0068ce342305c86e603381923e342e489d7
Sven Helmberger <sven.helmberger@quinscape.de>
2021-03-24 18:56:59 +0100

    pickSchemaTypes: Add selective queries / mutations
    The schemas contained no QueryType/MutationType handling until now.
    
    This adds a way to selectively add query and mutations methods to your
    schema.
    
        pickSchemaTypes(rawSchema, [ "q:iQueryFoo", "m:updateFoo" ])
    
    Here we pick only a query named "iQueryFoo" and a mutation named
    "updateFoo" and all their dependencies (return types and argument
    types). you can use "q:" as a prefix for mutations and "m:" as a prefix for
    mutations mixed with the normal type references.

--- src/util/pickSchemaTypes.js
+++ src/util/pickSchemaTypes.js
@@ -1,6 +1,25 @@
-import { unwrapAll, findNamed, getFields } from "./type-utils"
+import { findNamed, getFields, unwrapAll } from "./type-utils"
 
-import { SCALAR, INPUT_OBJECT, OBJECT} from "domainql-form/lib/kind"
+import { INPUT_OBJECT, OBJECT, SCALAR } from "domainql-form/lib/kind"
+
+
+function followField(field, rawSchema, knownTypes)
+{
+    const fieldTypeRef = unwrapAll(field.type);
+
+    if (fieldTypeRef.kind === OBJECT || fieldTypeRef.kind === INPUT_OBJECT)
+    {
+        followType(rawSchema, knownTypes, fieldTypeRef.name);
+    }
+    else if (fieldTypeRef.kind === SCALAR)
+    {
+        const typeDef = findNamed(rawSchema.types, fieldTypeRef.name);
+        if (typeDef != null && !knownTypes.has(fieldTypeRef.name))
+        {
+            knownTypes.add(fieldTypeRef.name);
+        }
+    }
+}
 
 
 function followType(rawSchema, knownTypes, typeName)
@@ -24,25 +43,23 @@ function followType(rawSchema, knownTypes, typeName)
         for (let i = 0; i < fields.length; i++)
         {
             const field = fields[i];
-            const fieldTypeRef = unwrapAll(field.type);
-
-            if (fieldTypeRef.kind === OBJECT || fieldTypeRef.kind === INPUT_OBJECT)
-            {
-                followType(rawSchema, knownTypes, fieldTypeRef.name);
-            }
-            else if (fieldTypeRef.kind === SCALAR)
-            {
-                const typeDef = findNamed(rawSchema.types, fieldTypeRef.name);
-                if (typeDef != null && !knownTypes.has(fieldTypeRef.name))
-                {
-                    knownTypes.add(fieldTypeRef.name);
-                }
-            }
+            followField(field, rawSchema, knownTypes);
         }
     }
-
 }
 
+
+const filterObject = (obj, knownTypes) => {
+    const out = {};
+    for (let key in obj)
+    {
+        if (obj.hasOwnProperty(key) && knownTypes.has(key))
+        {
+            out[key] = obj[key];
+        }
+    }
+    return out;
+}
 
 /**
  * Filters a raw input schema JSON structure to only include the types reachable by a given list of type names
@@ -55,32 +72,62 @@ export default function pickSchemaTypes(rawSchema, typeNames)
 {
     const knownTypes = new Set();
 
+    const queryType = { ... findNamed(rawSchema.types, "QueryType") };
+    const mutationType = { ... findNamed(rawSchema.types, "MutationType") };
+
+    const knownQueries = new Set();
+    const knownMutations = new Set();
+
     for (let i = 0; i < typeNames.length; i++)
     {
         const typeName = typeNames[i];
 
-        followType(rawSchema, knownTypes, typeName)
-    }
-
-    const filterObject = obj => {
-        const out = {};
-        for (let key in obj)
+        const isQueryRef = typeName.indexOf("q:") === 0;
+        const isMutationRef = typeName.indexOf("m:") === 0;
+        if (isQueryRef || isMutationRef)
         {
-            if (obj.hasOwnProperty(key) && knownTypes.has(key))
+            const fields = isQueryRef ? queryType.fields : mutationType.fields;
+            const { name, args, type} = findNamed( fields, typeName.substring(2));
+
+            for (let j = 0; j < args.length; j++)
             {
-                out[key] = obj[key];
+                followType(rawSchema, knownTypes, unwrapAll(args[j].type).name)
             }
+
+            followType(rawSchema, knownTypes, unwrapAll(type).name)
+
+            if (isQueryRef)
+            {
+                knownQueries.add(name);
+            }
+            else if (isMutationRef)
+            {
+                knownMutations.add(name);
+            }
+
         }
-        return out;
+        else
+        {
+            followType(rawSchema, knownTypes, typeName)
+        }
+
     }
 
     //console.log("TYPES:", [... knownTypes])
 
-    return {
+    const schema = {
         types: rawSchema.types.filter( td => knownTypes.has(td.name) ),
         relations: rawSchema.relations.filter( r => knownTypes.has(r.sourceType) && knownTypes.has(r.targetType)),
-        nameFields: filterObject(rawSchema.nameFields),
+        nameFields: filterObject(rawSchema.nameFields, knownTypes),
         genericTypes: rawSchema.genericTypes.filter( gt => knownTypes.has(gt.type))
-    }
+    };
+
+    queryType.fields = queryType.fields.filter( f => knownQueries.has(f.name));
+    mutationType.fields = mutationType.fields.filter( f => knownMutations.has(f.name));
+
+    schema.types.push(queryType);
+    schema.types.push(mutationType);
+
+    return schema
 
 }
