be7d2008d6fe853cf57ecce550cd859a1fc0bd0a
Denis Peters <denis.peters@quinscape.de>
2021-08-24 16:15:01 +0200

    Add optional user string to LogoutForm
    The user string will be shown on place of the default login id.
    It is possible to use a replacement string or a function for rendering
    react components.

--- src/ui/LogoutForm.js
+++ src/ui/LogoutForm.js
@@ -2,53 +2,63 @@ import React from "react"
 
 import config from "../config"
 import { Icon } from "domainql-form"
+import PropTypes from "prop-types";
 
 /**
  * A CSRF-protection-compliant Spring security log out-form
  */
-class LogoutForm extends React.Component {
+function LogoutForm({userString})
+{
 
-    render()
-    {
-        const { auth , contextPath, csrfToken } = config;
+    const { auth , contextPath, csrfToken } = config;
 
+    const { login } = auth;
 
-        const { login } = auth;
+    const isAnonymous = login === "anonymous";
 
-        const isAnonymous = login === "anonymous";
+    const jsonTitle = __DEV ? auth.roles.join(", ") : null;
 
-        const jsonTitle = __DEV ? auth.roles.join(", ") : null;
-
-        return (
-            <React.Fragment>
-                {
-                    !isAnonymous ?
-                    <form method="POST" action={ contextPath + "/logout" } className="form-inline fa-pull-right">
-                        { "Logged in as\u00a0" }
-                        <span className="text-primary">
+    return !isAnonymous ? 
+        (
+            <>
+                <form method="POST" action={ contextPath + "/logout" } className="form-inline fa-pull-right">
+                    { "Logged in as\u00a0" }
+                    <span className="text-primary">
 
                         <Icon className="fa-id-card-alt" title={ jsonTitle }/>
-                            { " " + login }
+                        { " " + (!userString ? login : (typeof userString === "function" ? userString() : userString)) }
                     </span>
-                        :
-                        <button type="submit" className="btn btn-link ">
-                            Log out
-                        </button>
-                        <input type="hidden" name={ csrfToken.param } value={ csrfToken.value }/>
-                    </form>
                     :
-                    <div className="fa-pull-right">
-                        <a
-                            className="btn btn-link"
-                            href={ contextPath + "/login" }
-                        >
-                            Login...
-                        </a>
-                    </div>
-                }
-            </React.Fragment>
+                    <button type="submit" className="btn btn-link ">
+                        Log out
+                    </button>
+                    <input type="hidden" name={ csrfToken.param } value={ csrfToken.value }/>
+                </form>
+            </>
         )
-    }
+        :
+        (
+            <>
+                <div className="fa-pull-right">
+                    <a
+                        className="btn btn-link"
+                        href={ contextPath + "/login" }
+                    >
+                        Login...
+                    </a>
+                </div>
+            </>
+        )
+}
+
+LogoutForm.propTypes = {
+    /**
+     * optional template for displaying the username reading
+     */
+     userString: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.func
+    ])
 }
 
 export default LogoutForm
