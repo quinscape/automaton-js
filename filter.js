4da14191a7082ed576489dea717dbbf99be7875f
Sven Helmberger <sven.helmberger@quinscape.de>
2021-10-06 20:12:05 +0200

    Add filter import helper
    The two-stage import via FilterDSL is a bit akward, and in some places
    I've already seen direct access of lib/FilterDSL, so I thought I add
    a new official way to import FilterDSL methods.
    
      import { and, or, field } from "@quinscape/automaton-js/filter"

---
+++ filter.js
@@ -0,0 +1,53 @@
+const FilterDSL = require("./lib/FilterDSL");
+const {
+
+    and,
+    or,
+    not,
+    condition,
+    operation,
+    field,
+    component,
+    value,
+    values,
+
+    Type,
+
+    isCondition,
+    isConditionObject,
+    getConditionArgCount,
+    isLogicalCondition,
+    isComposedComponentExpression,
+    findComponentNode,
+
+    FIELD_CONDITIONS,
+    CONDITION_METHODS,
+    FIELD_OPERATIONS
+} = FilterDSL;
+
+// base DSL methods
+exports.and = and;
+exports.or = or;
+exports.not = not;
+exports.condition = condition;
+exports.operation = operation;
+exports.field = field;
+exports.component = component;
+exports.value = value;
+exports.values = values;
+
+// condition types
+exports.Type = Type;
+
+// helper functions
+exports.isCondition = isCondition;
+exports.isConditionObject = isConditionObject;
+exports.getConditionArgCount = getConditionArgCount;
+exports.isLogicalCondition = isLogicalCondition;
+exports.isComposedComponentExpression = isComposedComponentExpression;
+exports.findComponentNode = findComponentNode;
+
+// reflection basically
+exports.FIELD_CONDITIONS = FIELD_CONDITIONS;
+exports.CONDITION_METHODS = CONDITION_METHODS;
+exports.FIELD_OPERATIONS = FIELD_OPERATIONS;
