20b4fe27976cba96607d631fc193699560b72c05
ZidArgs <zidargs@gmail.com>
2022-02-08 13:41:45 +0100

    Make useResizeObserver more efficient
    It is recommended to only add one ResizeObserver to the website.
    So one observer will now be used for all registering elements and the
    size setter will be determined by the referenced element.

--- src/util/useResizeObserver.js
+++ src/util/useResizeObserver.js
@@ -1,5 +1,23 @@
 import { useState, useEffect } from 'react';
 
+const REGISTRY = new WeakMap();
+const OBSERVER = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+        const refEl = entry.target;
+        handleResize(refEl);
+    }
+});
+
+function handleResize(refEl) {
+    if (refEl != null && REGISTRY.has(refEl)) {
+        const setElementSize = REGISTRY.get(refEl);
+        setElementSize({
+            width: refEl.offsetWidth,
+            height: refEl.offsetHeight,
+        });
+    }
+}
+
 /**
  * Helper to have a React hook for observing element sizes
  */
@@ -9,25 +27,20 @@ export default function useResizeObserver(ref) {
         height: undefined,
     });
 
-    function handleResize() {
-        const refEl = ref.current;
-        if (refEl != null) {
-            setElementSize({
-                width: refEl.offsetWidth,
-                height: refEl.offsetHeight,
-            });
-        }
-    }
-    
-    const observer = new ResizeObserver(handleResize);
-
     useEffect(() => {
         const refEl = ref.current;
         if (refEl != null) {
-            observer.observe(ref.current);
-            handleResize();
+            OBSERVER.observe(refEl);
+            REGISTRY.set(refEl, setElementSize);
+            handleResize(refEl);
         }
-        return () => observer.disconnect();
+        return () => {
+            const refEl = ref.current;
+            if (refEl != null) {
+                OBSERVER.unobserve(refEl);
+                REGISTRY.delete(refEl);
+            }
+        };
     }, [ref.current]);
 
     return elementSize;
