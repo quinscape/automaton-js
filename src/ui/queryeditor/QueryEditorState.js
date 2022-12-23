a85ea2f44e047e9f2d6ab52c438b73a6d1e826b1
Denis Peters <denis.peters@quinscape.de>
2022-12-23 15:40:43 +0100

    Improvements for QueryEditor
    To change the query editor to be more integrated, the onChange handler
    is introduced.
    This makes the save button optional. it will only be active, if the
    saveButtonOnClick handler is set.
    
    Additionally a data storage of any kind is now introduced to the query
    editor, it can be an observable storage or a simple object.
    
    The column selection is now causing a form error when it has no
    selection.

---
+++ src/ui/queryeditor/QueryEditorState.js
@@ -0,0 +1,77 @@
+import set from "lodash.set";
+import toPath from "lodash.topath";
+import {action, makeObservable, observable} from "mobx";
+
+export default class QueryEditorState {
+
+    /**
+     * Container observable
+     * @type {Object}
+     */
+    container = null;
+
+    /**
+     * Condition path within the container
+     * @type {String}
+     */
+    containerPath = null;
+
+    /**
+     * The starting GraphQL type for the condition
+     * @type {String}
+     */
+    rootType = null
+
+    /**
+     * Increased to signal query updates.
+     *
+     * @type {number}
+     */
+    @observable
+    updateCounter = 0;
+
+
+    constructor(rootType, container, containerPath)
+    {
+        makeObservable(this);
+        this.rootType = rootType;
+        this.container = container;
+        this.containerPath = containerPath;
+    }
+
+    get queryRoot()
+    {
+        const { container, containerPath } = this
+
+        return get(container, containerPath)
+    }
+
+    @action
+    setColumns(columns) {
+        const {container, containerPath} = this;
+
+        const p = toPath(containerPath).concat(toPath("columns"));
+        if (Array.isArray(columns) && columns.length > 0) {
+            set(container, p, columns);
+        } else {
+            set(container, p, null);
+        }
+
+        this.updateCounter++;
+    }
+
+    @action
+    setSort(sort) {
+        const {container, containerPath} = this;
+
+        const p = toPath(containerPath).concat(toPath("sort"));
+        if (Array.isArray(sort) && sort.length > 0) {
+            set(container, p, sort);
+        } else {
+            set(container, p, null);
+        }
+
+        this.updateCounter++;
+    }
+
+}
