96179692dda6a5a0ff0fbf7fcd3be2ee6c7c37b4
Marcel Dowideit <Marcel.Dowideit@quinscape.de>
2023-01-30 13:57:15 +0100

    Added exports

--- src/index.js
+++ src/index.js
@@ -90,7 +90,7 @@ import SelectionTreeModal from "./ui/treeselection/SelectionTreeModal"
 import QueryEditor from "./ui/queryeditor/QueryEditor";
 import ColumnSelect from "./ui/queryeditor/ColumnSelect";
 
-import { createTreeRepresentationForInputSchema } from "./util/inputSchemaUtilities"
+import { createTreeRepresentationForInputSchema,getTableNameByPath, getFieldDataByPath } from "./util/inputSchemaUtilities"
 
 import IconCell from "./ui/datagrid/iconcolumn/IconCell"
 import latestRequestInSession, { registerRequestForSession } from "./util/latestRequestInSession"
@@ -229,11 +229,14 @@ export {
     ColumnSelect,
 
     createTreeRepresentationForInputSchema,
+    getFieldDataByPath,
+    getTableNameByPath,
 
     IconCell,
     
     latestRequestInSession,
     registerRequestForSession
+
 }
 
 
