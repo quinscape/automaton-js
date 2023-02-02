055af928b9add49847234aa54c0ba7ce4a7fd46a
Denis Peters <denis.peters@quinscape.de>
2023-02-02 13:13:05 +0100

    Add sorted property to SelectionTree(Modal)
    Add sort functionality to the SelectionTree.
    This will sort the Tree by Text content, but folders will always be at
    the bottom.

--- src/ui/treeselection/SelectionTree.js
+++ src/ui/treeselection/SelectionTree.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Tree from "../tree/Tree";
 import PropTypes from "prop-types";
+
+const NODE_TYPE_ENTRY = "entry";
+const NODE_TYPE_DIRECTORY = "directory";
 
 const SelectionTree = (props) => {
     const {
@@ -10,8 +13,17 @@ const SelectionTree = (props) => {
         onExpandDirectory,
         onCollapseDirectory,
         valueRenderer,
-        singleSelect
+        singleSelect,
+        sorted
     } = props;
+
+    const preparedTree = useMemo(() => {
+        return prepareTree(treeContent, valueRenderer, "", sorted);
+    }, [
+        treeContent,
+        valueRenderer,
+        sorted
+    ]);
 
     return (
         <Tree
@@ -19,30 +31,69 @@ const SelectionTree = (props) => {
             onSelectedElementsChange={onSelectedElementsChange}
         >
             {
-                renderObject(treeContent, valueRenderer, singleSelect, onExpandDirectory, onCollapseDirectory)
+                renderTree(preparedTree, singleSelect, onExpandDirectory, onCollapseDirectory)
             }
         </Tree>
     )
 }
 
-function renderObject(
-    treeObject,
-    valueRenderer,
+function prepareTree(
+    treeContent,
+    valueRenderer, 
+    path = "",
+    sorted = false
+) {
+    const result = [];
+
+    for(const treeElement in treeContent) {
+        const newPath = path ? `${path}.${treeElement}` : treeElement;
+        if(typeof treeContent[treeElement] === "object") {
+            const value = renderNodeValue(valueRenderer, newPath, true);
+            const children = prepareTree(
+                treeContent[treeElement],
+                valueRenderer,
+                newPath,
+                sorted
+            );
+            result.push({
+                type: NODE_TYPE_DIRECTORY,
+                selectionId: newPath,
+                value,
+                children
+            });
+        } else {
+            const value = renderNodeValue(valueRenderer, newPath, false);
+            result.push({
+                type: NODE_TYPE_ENTRY,
+                selectionId: newPath,
+                value
+            });
+        }
+    }
+
+    if (sorted) {
+        result.sort(nodeComparator);
+    }
+
+    return result;
+}
+
+function renderTree(
+    treeContent,
     singleSelect,
     onExpandDirectory,
-    onCollapseDirectory,
-    path = ""
+    onCollapseDirectory
 ) {
     const renderedElements = [];
 
-    for(const treeElement in treeObject) {
-        const newPath = path ? `${path}.${treeElement}` : treeElement;
-        if(typeof treeObject[treeElement] === "object") {
+    for(const treeElement of treeContent) {
+        const {type, selectionId, value, children} = treeElement;
+        if (type === NODE_TYPE_DIRECTORY) {
             renderedElements.push(
                 <Tree.TreeNode
-                    key={newPath}
-                    selectionId={newPath}
-                    renderer={valueRenderer}
+                    key={selectionId}
+                    selectionId={selectionId}
+                    renderer={value}
                     renderCheckbox
                     singleSelect={singleSelect}
                     forceDirectory
@@ -50,13 +101,11 @@ function renderObject(
                     onCollapseDirectory={onCollapseDirectory}
                 >
                     {
-                        renderObject(
-                            treeObject[treeElement],
-                            valueRenderer,
+                        renderTree(
+                            children,
                             singleSelect,
                             onExpandDirectory,
-                            onCollapseDirectory,
-                            newPath
+                            onCollapseDirectory
                         )
                     }
                 </Tree.TreeNode>
@@ -64,9 +113,9 @@ function renderObject(
         } else {
             renderedElements.push(
                 <Tree.TreeNode
-                    key={newPath}
-                    selectionId={newPath}
-                    renderer={valueRenderer}
+                    key={selectionId}
+                    selectionId={selectionId}
+                    renderer={value}
                     renderCheckbox
                     singleSelect={singleSelect}
                 />
@@ -75,6 +124,46 @@ function renderObject(
     }
 
     return renderedElements;
+}
+
+function renderNodeValue(renderer, selectionId, isDirectory) {
+    return typeof renderer === "function" ? renderer(selectionId, {
+        isTree: true,
+        isDirectory
+    }) : renderer ?? selectionId
+}
+
+function nodeComparator(a, b) {
+    const {type: aType, value: aValue} = a;
+    const {type: bType, value: bValue} = b;
+    if (aType === bType) {
+        return extractText(aValue).toLowerCase().localeCompare(extractText(bValue).toLowerCase());
+    }
+    if (aType === NODE_TYPE_ENTRY) {
+        return -1;
+    }
+    if (aType === NODE_TYPE_DIRECTORY) {
+        return 1;
+    }
+}
+
+function extractText(node) {
+    if (node == null || typeof node === "boolean") {
+        return "";
+    }
+    if (typeof node === "number") {
+        return node.toString();
+    }
+    if (typeof node === "string") {
+        return node;
+    }
+    if (Array.isArray(node)) {
+        return node.map(extractText).join("");
+    }
+    if (node.props != null && "children" in node.props) {
+        return extractText(node.props.children);
+    }
+    return "";
 }
 
 SelectionTree.propTypes = {
@@ -111,7 +200,13 @@ SelectionTree.propTypes = {
     /**
      * if the tree is in single select mode
      */
-    singleSelect: PropTypes.bool
+    singleSelect: PropTypes.bool,
+
+    /**
+     * if the tree will be sorted
+     * sorting first by type, to enforce directories to the bottom, then by inner text
+     */
+    sorted: PropTypes.bool
 }
 
 export default SelectionTree;
