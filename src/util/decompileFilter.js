10a1a476e7ece510725c8ff50c9dc8a63703f343
Sven Helmberger <sven.helmberger@quinscape.de>
2023-03-22 14:20:26 +0100

    decompileFilter: Support marking sub objects
    Introduce a third parameter "match" that references another part of the
    same condition graph. The source output of that reference is enclosed
    with /*>>*/  /*<<*/ to mark the object.

--- src/util/decompileFilter.js
+++ src/util/decompileFilter.js
@@ -1,5 +1,4 @@
 import { COMPUTED_VALUE_TYPE, Type } from "../FilterDSL"
-import config from "../config";
 
 function convert(value, scalarType)
 {
@@ -38,8 +37,20 @@ const simplifiedValues = {
     "Timestamp": true
 }
 
-export default function decompileFilter(condition, level = 0, invert = true)
-{
+
+/**
+ * Converts the given FilterDSL condition graph into a pretty-formatted source
+ * form using the FilterDSL API (and DateTime.fromIso() expressions)
+ *
+ * @param {Object} condition        condition graph
+ * @param {Number} level            starting indentation level
+ * @param {Object} match            Optional object to mark. The given node is assumed to an instance in the graph and
+ *                                  the source output of that object is marked is enclosed in \/*>>*\/ \/*<<*\/ to mark the object
+ * @param {boolean} invert          inverts condition output (mostly internal usage)
+ *
+ * @return {string} pretty-printed source string. If match was used, the >> << might prevent it from being valid JavaScript
+ */
+export default function decompileFilter(condition, level = 0, match = null, invert = true) {
     if (!condition)
     {
         return indent(level) + "null";
@@ -61,39 +72,42 @@ export default function decompileFilter(condition, level = 0, invert = true)
 
     const nextLevel = level >= 0 ? level + 1 : level
 
+    const markerL = match === condition ? "/*>>*/ " : "";
+    const markerR = match === condition ? " /*<<*/" : "";
+
     if (type === Type.FIELD)
     {
-        return indent(level) + "field(" + JSON.stringify(name) + ")";
+        return indent(level) + markerL + "field(" + JSON.stringify(name) + ")" + markerR;
     }
     else if (type === Type.CONDITION || type === Type.OPERATION)
     {
         const { name, operands } = condition;
         if (invert && !topLevelConditions.hasOwnProperty(name))
         {
-            return decompileFilter( operands[0], level, true) + "." + name + (level >= 0 && operands.length > 1 ? "(\n" : "(") + operands.slice(1).map(o => decompileFilter(o, nextLevel, invert)).join((level >= 0 && operands.length > 1? ",\n" : ",")) + (level >= 0 && operands.length > 1 ? "\n" : "") + indent(level) + ")";
+            return decompileFilter(operands[0], level, match, true) + "." + markerL + name + (level >= 0 && operands.length > 1 ? "(\n" : "(") + operands.slice(1).map(o => decompileFilter(o, nextLevel, match, invert)).join((level >= 0 && operands.length > 1? ",\n" : ",")) + (level >= 0 && operands.length > 1 ? "\n" : "") + indent(level) + ")" + markerR;
         }
-        return indent(level) + name + (level >= 0 && operands.length > 0 ? "(\n" : "(") + operands.map( o => decompileFilter(o, nextLevel, invert)).join((level >= 0 && operands.length > 1 ? ",\n" : ",")) + (level >= 0 && operands.length > 0? "\n" : "") + indent(level) + ")";
+        return indent(level) + markerL + name + (level >= 0 && operands.length > 0 ? "(\n" : "(") + operands.map( o => decompileFilter(o, nextLevel, match, invert)).join((level >= 0 && operands.length > 1 ? ",\n" : ",")) + (level >= 0 && operands.length > 0? "\n" : "") + indent(level) + ")" + markerR;
     }
     else if (type === Type.VALUE)
     {
         const { value, scalarType } = condition;
         if (value !== null && simplifiedValues[scalarType])
         {
-            return indent(level) + "value(" + convert(value, scalarType) + ")";
+            return indent(level) + markerL +"value(" + convert(value, scalarType) + ")" + markerR;
         }
-        return indent(level) + "value(" + JSON.stringify(value) + ", " + JSON.stringify(scalarType) +")";
+        return indent(level) + markerL +"value(" + JSON.stringify(value) + ", " + JSON.stringify(scalarType) +")" + markerR;
 
     }
     else if (type === Type.VALUES)
     {
         const { values, scalarType } = condition;
-        return indent(level) + "values(" + JSON.stringify(scalarType) + ", " + JSON.stringify(values)+ ")";
+        return indent(level) + markerL +"values(" + JSON.stringify(scalarType) + ", " + JSON.stringify(values)+ ")" + markerR;
 
     }
     else if (type === Type.COMPONENT)
     {
         const { id, condition : component } = condition;
-        return indent(level) + "component(" + JSON.stringify(id) + ", " + (level >= 0 ? "\n" : "") + decompileFilter(component, nextLevel) + (level >= 0 ? "\n" : "") + indent(level) + ")";
+        return indent(level) + markerL + "component(" + JSON.stringify(id) + ", " + (level >= 0 ? "\n" : "") + decompileFilter(component, nextLevel, match, true) + (level >= 0 ? "\n" : "") + indent(level) + ")" + markerR;
     }
     else
     {
