3c8e0fb90fae42ee8f68c7e1c4a59cd4d71e82d0
Denis Peters <denis.peters@quinscape.de>
2023-06-06 12:30:39 +0200

    Add doc

--- src/ui/datagrid/GridStateFormContext.js
+++ src/ui/datagrid/GridStateFormContext.js
@@ -1,6 +1,9 @@
 import { createContext } from "react";
 import { action, makeObservable, observable } from "mobx";
 
+/**
+ * A ContextState for the GridStateForm containing the filter values for its DataGrid.
+ */
 export class GridStateFormContextState {
 
     filters = observable.array([]);
