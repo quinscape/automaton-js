d0e05713925941e26fae6978d5bd0df7852c82f3
Denis Weiß <denis.peters@quinscape.de>
2024-06-18 09:39:46 +0200

    hide caret button instead of not rendering it to keep indentation of tree nodes

--- src/ui/tree/CaretButton.js
+++ src/ui/tree/CaretButton.js
@@ -1,15 +1,16 @@
 import { Icon } from "domainql-form";
 import React from "react";
+import cx from "classnames";
 import i18n from "../../i18n";
 
 const ICON_OPEN = <Icon className="fa-caret-down"/>;
 const ICON_CLOSE = <Icon className="fa-caret-right"/>;
 
 
-const CaretButton = ({open, onClick}) => (
+const CaretButton = ({open, onClick, invisible}) => (
     <button
         type="button"
-        className="caret btn btn-link"
+        className={cx("caret", "btn", "btn-link", invisible && "invisible")}
         aria-expanded={open}
         tabIndex={-1}
         onClick={onClick}
